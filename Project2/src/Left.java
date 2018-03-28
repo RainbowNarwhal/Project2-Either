0e4cf002eda286ae12901ac5b4b75dd454f0371a
km1473 <km1473@mct162l08>
2018-03-28 09:19:13 -0400

    added comments

--- Project2/src/Left.java
+++ Project2/src/Left.java
@@ -2,7 +2,6 @@
  * This is a class to represent the "empty" in optional. This class will get
  * treated as an error and will be handled as such. Our .map function will ignore all the Left values.
  * @author km1473
- *
  */
 public class Left extends Either
 {
--- Project2/src/Right.java
+++ Project2/src/Right.java
@@ -1,7 +1,6 @@
 /**
  * This is a class to represent the Optional<T> in the Optional class.
  * @author km1473
- *
  */
 
 public class Right extends Either
