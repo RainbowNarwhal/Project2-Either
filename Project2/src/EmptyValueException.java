1bc5d9ba61187d219d44447447db9cdeaac5e12e
km1473 <km1473@mct164l07>
2018-04-04 14:47:34 -0400

    Now actually did what I said

---
+++ Project2/src/EmptyValueException.java
@@ -0,0 +1,15 @@
+/**
+ * This class handles when our "optional" is empty. Shows what went wrong and ends the execution
+ * @author km1473
+ */
+public class EmptyValueException extends Exception
+{
+	String message;
+	public EmptyValueException(String message)
+	{
+		this.message = message;
+		//message = "The value in this variable is null";
+		System.out.println(message);
+		System.exit(1);
+	}
+}
